366ecc0a004892994d85d078134268f989b1a89a
simsim314 <simsim314@gmail.com>
2016-05-16 22:51:18 +0300

    LifeTile solution first commit

---
+++ LifeTile/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace LifeTile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 819);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(317, 819);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button2_KeyPress);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox2.Location = new System.Drawing.Point(238, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1371, 813);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = global::LifeTile.Properties.Resources._16tiles;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(232, 854);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1611, 854);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ LifeTile/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifeTile
+{
+    public partial class Form1 : Form, IMessageFilter
+    {
+        int xS = -1, yS = -1;
+        int targetX = -1, targetY = -1;
+        int kx = 0, ky = 0; 
+
+        Bitmap image2;
+        Bitmap imageCroped;
+        float scaleDown = 0.7f;
+        int curMode = 0;
+        int latestIdx = 0;
+        const int WM_KEYDOWN = 0x100;
+        const int WM_KEYUP = 0x101;
+        const int WM_SYSKEYDOWN = 0x104;
+        Keys lastKeyPressed = Keys.None;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Application.AddMessageFilter(this);
+            this.FormClosed += (s, e) => Application.RemoveMessageFilter(this);
+        }
+
+        void DrawGrid()
+        {
+             Bitmap default_image = new Bitmap(pictureBox1.Image);
+             default_image.MakeTransparent(Color.White);
+
+            pictureBox1.Image = default_image;
+
+            using (Graphics gr = Graphics.FromImage(pictureBox1.Image))
+            {
+
+                int dx = pictureBox1.Image.Width / 2;
+                int dy = pictureBox1.Image.Height / 8;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        gr.DrawRectangle(Pens.Black, dx * j, dy * i, dx - 2, dy - 2);
+
+                        if(i == xS && j == yS)
+                            gr.DrawRectangle(Pens.Red, dx * j, dy * i, dx - 2, dy - 2);
+
+                    }
+                }
+
+
+                
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            image2 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+
+            int dx = pictureBox1.Image.Width / 2;
+            int dy = pictureBox1.Image.Height / 8;
+            imageCroped = new Bitmap((int)(10 + dx * scaleDown), (int)(10 + dy * scaleDown));
+
+            using (Graphics g = Graphics.FromImage(image2))
+            {
+                g.FillRectangle(new SolidBrush(Color.White), 0, 0, pictureBox2.Width, pictureBox2.Height);
+               
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //bmp.RotateFlip(RotateFlipType.Rotate180FlipX);
+            //bmp.MakeTransparent(Color.Black);
+            //bmp.Save("test.bmp");
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+              int dx = pictureBox1.Width / 2;
+              int dy = pictureBox1.Height / 8;
+             
+                for (int i = 0; i < 8; i++)
+                    for (int j = 0; j < 2; j++)
+                        if (e.X > j * dx && e.X - dx < j * dx && e.Y > i * dy && e.Y - dy < i * dy)
+                        {
+                            xS = i;
+                            yS = j;
+                        }
+
+                this.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGrid();
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            int dx = pictureBox1.Image.Width / 2;
+            int dy = pictureBox1.Image.Height / 8;
+            
+            e.Graphics.DrawImage(image2, 0, 0);
+
+            
+            int width = dx;
+            int height = dy;
+           RectangleF destinationRectCrop = new RectangleF(
+               5,
+               5,
+               scaleDown * dx,
+               scaleDown * dy);
+
+            // Draw a portion of the image. Scale that portion of the image
+
+            using (Graphics g = Graphics.FromImage(imageCroped))
+            {
+                g.Clear(Color.FromArgb(0, 0, 0, 0));
+                if (curMode >= 8)
+                {
+                    Matrix mat = new Matrix();
+                    var p = new PointF(scaleDown * dx / 2f - 1, scaleDown * dy / 2f - 1);
+                    mat.RotateAt(45, p);
+                    g.Transform = mat;
+                }
+
+                g.InterpolationMode = InterpolationMode.High;
+            
+                RectangleF sourceRect = new RectangleF(dx * yS + 3, dy * xS + 3, dx - 6, dy - 6);
+                g.DrawImage(
+                    pictureBox1.Image,
+                    destinationRectCrop,
+                    sourceRect,
+                    GraphicsUnit.Pixel);
+            }
+
+            imageCroped.RotateFlip((RotateFlipType)(curMode % 8));
+            e.Graphics.DrawImage(imageCroped, targetX - (int)(scaleDown * dx / 2) + kx, targetY - (int)(scaleDown * dy / 2) + ky);
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            targetX = e.X;
+            targetY = e.Y;
+            pictureBox2.Refresh();
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                curMode++;
+
+                if (curMode >= 16)
+                    curMode = 0;
+
+                Text = "Mode = " + curMode.ToString();
+            }
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Graphics g = Graphics.FromImage(image2);
+                int dx = pictureBox1.Image.Width / 2;
+                int dy = pictureBox1.Image.Height / 8;
+
+                g.DrawImage(imageCroped, targetX - (int)(scaleDown * dx / 2) + kx, targetY - (int)(scaleDown * dy / 2) + ky);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            image2.Save("latestIdx" + (latestIdx++).ToString() + ".png");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            latestIdx = -1;
+
+            while (File.Exists("latestIdx" + (++latestIdx).ToString() + ".png")) ;
+            
+            if(latestIdx > 0)
+                image2 = new Bitmap("latestIdx" + (latestIdx - 1).ToString() + ".png");
+
+        }
+
+        private void button2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        public bool PreFilterMessage(ref Message m)
+        {
+            return false;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if ((msg.Msg == WM_KEYDOWN) || (msg.Msg == WM_SYSKEYDOWN))
+            {
+                lastKeyPressed = keyData;
+                switch (keyData)
+                {
+                    case Keys.Down:
+                        ky++;
+                        this.Refresh();
+                        break;
+
+                    case Keys.Up:
+                        ky--;
+                        this.Refresh();
+                        break;
+
+                    case Keys.Left:
+                        kx--;
+                        this.Refresh();
+                        break;
+
+                    case Keys.Right:
+                        kx++;
+                        this.Refresh();
+                        break;
+
+                }
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
